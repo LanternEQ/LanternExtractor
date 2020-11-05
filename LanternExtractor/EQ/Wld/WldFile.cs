cc64e39cd79a0c2d0066ccb639e8a6de0f43ced4
Daniel Wilkins <wilkins.c.daniel@gmail.com>
2020-11-04 22:57:23 -0800

    Add test method for creating new vertex colors

--- LanternExtractor/EQ/Wld/WldFile.cs
+++ LanternExtractor/EQ/Wld/WldFile.cs
@@ -116,7 +116,10 @@ namespace LanternExtractor.EQ.Wld
             _bspRegions = new List<BspRegion>();
             
             var reader = new BinaryReader(new MemoryStream(_wldFile.Bytes));
-            var writer = new BinaryWriter(new MemoryStream(_wldFile.Bytes));
+            
+            byte[] writeBytes = reader.ReadBytes(_wldFile.Bytes.Length);
+            reader.BaseStream.Position = 0;
+            var writer = new BinaryWriter(new MemoryStream(writeBytes));
 
             int identifier = reader.ReadInt32();
 
@@ -173,6 +176,10 @@ namespace LanternExtractor.EQ.Wld
 
                 readPosition = reader.BaseStream.Position;
 
+                if (fragId == FragmentType.SkeletonHierarchy && i == 125)
+                {
+                }
+
                 newFragment.Initialize(i, fragId, (int) fragSize, reader.ReadBytes((int) fragSize), _fragments, _stringHash,
                     _isNewWldFormat,
                     _logger);
@@ -195,6 +202,36 @@ namespace LanternExtractor.EQ.Wld
                     _bspRegions.Add(newFragment as BspRegion);
                 }
 
+                long cachedPosition = reader.BaseStream.Position;
+                // Create data mods class
+                if (_wldType == WldType.Zone && newFragment.Type == FragmentType.Mesh)
+                {
+                    // Get vertex color count
+                    int skip = 18 * 4 + 3 * 2;
+
+                    long colorCountLocation = readPosition + skip;
+
+                    reader.BaseStream.Position = colorCountLocation;
+
+                    int count = reader.ReadInt16();
+
+                    long colorsLocation = readPosition + (newFragment as Mesh).ColorStart;
+                    reader.BaseStream.Position = colorsLocation;
+                    writer.BaseStream.Position = colorsLocation;
+
+                    Random random = new Random();
+                    for (int j = 0; j < count; ++j)
+                    {
+                        var dwordValue = reader.ReadInt32();
+                        var bytesValue = BitConverter.GetBytes(dwordValue);
+                        byte amount = 255;//(byte)((random.Next(int.MaxValue) % 2 == 0) ? 0 : 255);
+                        bytesValue[3] = amount;
+                        writer.Write(bytesValue);
+                    }
+                }
+
+                reader.BaseStream.Position = cachedPosition;
+
                 _fragmentTypeDictionary[fragId].Add(newFragment);
             }
 
@@ -208,12 +245,9 @@ namespace LanternExtractor.EQ.Wld
                 ExportData();
             }
 
-            if (_wldType == WldType.Objects)
+            if (_wldType == WldType.Zone)
             {
-                var fileStream = File.Create("objects.wld");
-                writer.BaseStream.Seek(0, SeekOrigin.Begin);
-                writer.BaseStream.CopyTo(fileStream);
-                fileStream.Close();
+                File.WriteAllBytes("arena.wld", writeBytes);
             }
             
             return true;
