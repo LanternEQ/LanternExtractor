0543e89b950e01d11c756daa346d7f1a99ecc2ef
Daniel Wilkins <wilkins.c.daniel@gmail.com>
2021-08-05 23:58:54 -0700

    Add MaterialFixer for resolving shader bugs

---
+++ LanternExtractor/EQ/Wld/Helpers/MaterialFixer.cs
@@ -0,0 +1,38 @@
+﻿using LanternExtractor.EQ.Wld.Fragments;
+
+namespace LanternExtractor.EQ.Wld.Helpers
+{
+    /// <summary>
+    /// Fixes issues with EverQuest materials.
+    /// Currently, only fixes incorrect shader assignment
+    /// but can be extended in the future.
+    /// </summary>
+    public static class MaterialFixer
+    {
+        public static void Fix(WldFile wldFile)
+        {
+            FixShaderAssignment(wldFile);
+        }
+
+        private static void FixShaderAssignment(WldFile wld)
+        {
+            var materialFragments = wld.GetFragmentsOfType<Material>();
+
+            foreach (var mf in materialFragments)
+            {
+                switch (mf.Name)
+                {
+                    case "TREE20_MDF":
+                    case "TOP_MDF":
+                    case "FURPILE1_MDF":
+                    case "BEARRUG_MDF":
+                        mf.ShaderType = ShaderType.TransparentMasked;
+                        break;
+                    case "FIRE1_MDF":
+                        mf.ShaderType = ShaderType.TransparentAdditiveUnlit;
+                        break;
+                }
+            }
+        }
+    }
+}
