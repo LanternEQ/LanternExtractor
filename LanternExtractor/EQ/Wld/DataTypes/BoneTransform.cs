c51fed361ec0f56c32e345f5a34863cd5d959126
Paul Johnson <pcj174@gmail.com>
2021-11-17 07:31:46 -0500

    sync

---
+++ LanternExtractor/EQ/Wld/DataTypes/BoneTransform.cs
@@ -0,0 +1,12 @@
+﻿using GlmSharp;
+
+namespace LanternExtractor.EQ.Wld.DataTypes
+{
+    public class BoneTransform
+    {
+        public vec3 Translation { get; set; }
+        public quat Rotation { get; set; }
+        public float Scale { get; set; }
+        public mat4 ModelMatrix;
+    }
+}
