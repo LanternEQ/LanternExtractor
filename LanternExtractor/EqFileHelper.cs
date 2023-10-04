LanternEQ/LanternExtractor

commit 9e67b3ca09589326f93967ea0a3baceeb1c91ff8

Rework export settings and fix archive location

LanternExtractor/EqFileHelper.cs
@@ -6,6 +6,137 @@ namespace LanternExtractor
 {
     public static class EqFileHelper
     {
+        public static List<string> GetValidEqFilePaths(string directory, string archiveName)
+        {
+            archiveName = archiveName.ToLower();
+            var eqFiles = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+            List<string> validFiles;
+
+            switch (archiveName)
+            {
+                case "all":
+                    validFiles = GetAllValidFiles(eqFiles);
+                    break;
+                case "zones":
+                    validFiles = GetValidZoneFiles(eqFiles);
+                    break;
+                case "characters":
+                    validFiles = GetValidCharacterFiles(eqFiles);
+                    break;
+                case "equipment":
+                    validFiles = GetValidEquipmentFiles(eqFiles);
+                    break;
+                case "sounds":
+                    validFiles = GetValidSoundFiles(eqFiles);
+                    break;
+                default:
+                {
+                    validFiles = GetValidFiles(archiveName, directory);
+                    break;
+                }
+            }
+
+            return validFiles;
+        }
+
+        private static List<string> GetValidEquipmentFiles(string[] eqFiles)
+        {
+            return eqFiles.Where(x => IsEquipmentArchive(Path.GetFileName(x))).ToList();
+        }
+
+        private static List<string> GetAllValidFiles(string[] eqFiles)
+        {
+            return eqFiles.Where(x => IsValidArchive(Path.GetFileName(x))).ToList();
+        }
+
+        private static List<string> GetValidZoneFiles(string[] eqFiles)
+        {
+            return eqFiles.Where(x => IsZoneArchive(Path.GetFileName(x))).ToList();
+        }
+
+        private static List<string> GetValidCharacterFiles(string[] eqFiles)
+        {
+            return eqFiles.Where(x => IsCharacterArchive(Path.GetFileName(x))).ToList();
+        }
+
+        private static List<string> GetValidSoundFiles(string[] eqFiles)
+        {
+            return eqFiles.Where(x => IsSoundArchive(Path.GetFileName(x))).ToList();
+        }
+
+        private static List<string> GetValidFiles(string archiveName, string directory)
+        {
+            var validFiles = new List<string>();
+            if (archiveName.EndsWith(".s3d") || archiveName.EndsWith(".pfs"))
+            {
+                string archivePath = Path.Combine(directory, archiveName);
+                if (File.Exists(archivePath))
+                {
+                    validFiles.Add(archivePath);
+                }
+            }
+            else
+            {
+                string archivePath = Path.Combine(directory, $"{archiveName}.pfs");
+
+                if (File.Exists(archivePath))
+                {
+                    validFiles.Add(archivePath);
+                    return validFiles;
+                }
+
+                // Try and find all associated files with the shortname - can theoretically be a non-zone file
+                string mainArchivePath = Path.Combine(directory, $"{archiveName}.s3d");
+                if (File.Exists(mainArchivePath))
+                {
+                    validFiles.Add(mainArchivePath);
+                }
+
+                // Some zones have additional object archives for things added past their initial release
+                // No archives contain cross archive fragment references
+                string extensionObjectsArchivePath = Path.Combine(directory, $"{archiveName}_2_obj.s3d");
+                if (File.Exists(extensionObjectsArchivePath))
+                {
+                    validFiles.Add(extensionObjectsArchivePath);
+                }
+
+                string objectsArchivePath = Path.Combine(directory, $"{archiveName}_obj.s3d");
+                if (File.Exists(objectsArchivePath))
+                {
+                    validFiles.Add(objectsArchivePath);
+                }
+
+                string charactersArchivePath = Path.Combine(directory, $"{archiveName}_chr.s3d");
+                if (File.Exists(charactersArchivePath))
+                {
+                    validFiles.Add(charactersArchivePath);
+                }
+
+                // Some zones have additional character archives for things added past their initial release
+                // None of them contain fragments that are linked to other related archives.
+                // "qeynos" must be excluded because both qeynos and qeynos2 are used as shortnames
+                string extensionCharactersArchivePath = Path.Combine(directory, $"{archiveName}2_chr.s3d");
+                if (File.Exists(extensionCharactersArchivePath) && archiveName != "qeynos")
+                {
+                    validFiles.Add(extensionCharactersArchivePath);
+                }
+            }
+
+            return validFiles;
+        }
+
+        private static bool IsValidArchive(string archiveName)
+        {
+            return archiveName.EndsWith(".s3d") || archiveName.EndsWith(".pfs") && !archiveName.Contains("chequip") &&
+                !archiveName.EndsWith("_lit.s3d");
+        }
+
+        private static bool IsZoneArchive(string archiveName)
+        {
+            return IsValidArchive(archiveName) && !IsEquipmentArchive(archiveName) && !IsSkyArchive(archiveName) &&
+                   !IsBitmapArchive(archiveName) && !IsCharacterArchive(archiveName);
+        }
+
         public static bool IsEquipmentArchive(string archiveName)
         {
             return archiveName.StartsWith("gequip");
@@ -13,7 +144,8 @@ namespace LanternExtractor
 
         public static bool IsCharacterArchive(string archiveName)
         {
-            return archiveName.Contains("_chr") || archiveName.StartsWith("chequip") || archiveName.Contains("_amr");
+            return archiveName.Contains("_chr") || archiveName.StartsWith("chequip") ||
+                                                   archiveName.Contains("_amr");
         }
 
         public static bool IsObjectsArchive(string archiveName)
@@ -26,6 +158,11 @@ namespace LanternExtractor
             return archiveName == "sky";
         }
 
+        public static bool IsBitmapArchive(string archiveName)
+        {
+            return archiveName.StartsWith("bmpwad");
+        }
+
         public static bool IsSoundArchive(string archiveName)
         {
             return archiveName.StartsWith("snd");
@@ -35,7 +172,7 @@ namespace LanternExtractor
         {
             return archiveName == "clientdata";
         }
-        
+
         public static bool IsMusicFile(string filename)
         {
             return filename.EndsWith(".xmi");
@@ -46,88 +183,20 @@ namespace LanternExtractor
             return archiveName == "clientdata" || archiveName == "music";
         }
 
-        public static List<string> GetValidEqFilePaths(string directory, string archiveName)
+        public static bool IsUsedSoundArchive(string archiveName)
         {
-            archiveName = archiveName.ToLower();
-            var validFiles = new List<string>();
-            var eqFiles = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
-
-            if (archiveName == "all")
+            if (!IsSoundArchive(archiveName))
             {
-                validFiles = eqFiles.Where(f => (f.EndsWith(".s3d") || f.EndsWith(".pfs")) && !f.Contains("chequip") &&
-                                                !f.EndsWith("_lit.s3d")).ToList();
-            }
-            if (archiveName == "characters")
-            {
-                validFiles = eqFiles.Where(IsCharacterArchive).ToList();
-            }
-            else if (archiveName == "equipment")
-            {
-                validFiles = eqFiles.Where(IsEquipmentArchive).ToList();
-            }
-            else if (archiveName == "sounds")
-            {
-                validFiles = eqFiles.Where(IsSoundArchive).ToList();
-            }
-            else if (archiveName.EndsWith(".s3d") || archiveName.EndsWith(".pfs"))
-            {
-                string archivePath = directory + archiveName;
-
-                if (File.Exists(archivePath))
-                {
-                    validFiles.Add(archivePath);
-                }
-            }
-            else
-            {
-                // If it's the shortname of a PFS file, we assume it's standalone - used for sound files
-                string archivePath = directory + archiveName + ".pfs";
-
-                if (File.Exists(archivePath))
-                {
-                    validFiles.Add(archivePath);
-
-                    return validFiles;
-                }
-
-                // Try and find all associated files with the shortname - can theoretically be a non zone file
-                string mainArchivePath = directory + archiveName + ".s3d";
-                if (File.Exists(mainArchivePath))
-                {
-                    validFiles.Add(mainArchivePath);
-                }
-
-                // Some zones have additional object archives for things added past their initial release
-                // None of them contain fragments that are linked to other related archives.
-                string extensionObjectsArchivePath = directory + archiveName + "_2_obj.s3d";
-                if (File.Exists(extensionObjectsArchivePath))
-                {
-                    validFiles.Add(extensionObjectsArchivePath);
-                }
-
-                string objectsArchivePath = directory + archiveName + "_obj.s3d";
-                if (File.Exists(objectsArchivePath))
-                {
-                    validFiles.Add(objectsArchivePath);
-                }
-
-                string charactersArchivePath = directory + archiveName + "_chr.s3d";
-                if (File.Exists(charactersArchivePath))
-                {
-                    validFiles.Add(charactersArchivePath);
-                }
-
-                // Some zones have additional character archives for things added past their initial release
-                // None of them contain fragments that are linked to other related archives.
-                // "qeynos" must be excluded because both qeynos and qeynos2 are used as shortnames
-                string extensionCharactersArchivePath = directory + archiveName + "2_chr.s3d";
-                if (File.Exists(extensionCharactersArchivePath) && archiveName != "qeynos")
-                {
-                    validFiles.Add(extensionCharactersArchivePath);
-                }
+                return false;
             }
 
-            return validFiles;
+            // Trilogy client does not use archives higher than snd9
+            if (int.TryParse(archiveName.Substring(archiveName.Length - 2), out _))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }

LanternExtractor/EQ/ArchiveExtractor.cs
@@ -36,15 +36,15 @@ namespace LanternExtractor.EQ
 
             // For non WLD files, we can just extract their contents
             // Used for pure texture archives (e.g. bmpwad.s3d) and sound archives (e.g. snd1.pfs)
-            // The difference between this and the raw export is that it will convert images to .png
+            // The difference between this and the raw export is that it will convert images to PNG
             if (!s3dArchive.IsWldArchive)
             {
                 WriteS3dTextures(s3dArchive, rootFolder + shortName, logger);
 
-                if (EqFileHelper.IsSoundArchive(archiveName))
+                if (EqFileHelper.IsUsedSoundArchive(archiveName))
                 {
-                    var soundFolder = settings.ExportSoundsToSingleFolder ? "sounds" : shortName;
-                    WriteS3dSounds(s3dArchive, rootFolder + soundFolder, logger);
+                    WriteS3dSounds(s3dArchive,
+                        Path.Combine(rootFolder, settings.ExportSoundsToSingleFolder ? "sounds" : shortName), logger);
                 }
 
                 return;
@@ -70,7 +70,8 @@ namespace LanternExtractor.EQ
             }
             else if (EqFileHelper.IsCharacterArchive(archiveName))
             {
-                ExtractArchiveCharacters(path, rootFolder, logger, settings, archiveName, wldFileInArchive, shortName, s3dArchive);
+                ExtractArchiveCharacters(path, rootFolder, logger, settings, archiveName, wldFileInArchive, shortName,
+                    s3dArchive);
             }
             else if (EqFileHelper.IsObjectsArchive(archiveName))
             {
@@ -82,7 +83,6 @@ namespace LanternExtractor.EQ
             }
 
             MissingTextureFixer.Fix(archiveName);
-
         }
 
         private static void ExtractArchiveZone(string path, string rootFolder, ILogger logger, Settings settings,
@@ -104,7 +104,8 @@ namespace LanternExtractor.EQ
                 if (litWldLightsFileInArchive != null)
                 {
                     var lightsWldFile =
-                        new WldFileLights(litWldLightsFileInArchive, shortName, WldType.Lights, logger, settings, wldFileLit);
+                        new WldFileLights(litWldLightsFileInArchive, shortName, WldType.Lights, logger, settings,
+                            wldFileLit);
                     lightsWldFile.Initialize(rootFolder);
                 }
             }
@@ -120,6 +121,7 @@ namespace LanternExtractor.EQ
                 wldFile.RootFolder = rootFolder;
                 wldFile.ShortName = shortName;
             }
+
             InitializeWldAndWriteTextures(wldFile, rootFolder, rootFolder + shortName + "/Zone/Textures/",
                 s3dArchive, settings, logger);
 
@@ -200,7 +202,8 @@ namespace LanternExtractor.EQ
                 s3dArchive, settings, logger);
         }
 
-        private static void ExtractArchiveSky(string rootFolder, ILogger logger, Settings settings, PfsFile wldFileInArchive,
+        private static void ExtractArchiveSky(string rootFolder, ILogger logger, Settings settings,
+            PfsFile wldFileInArchive,
             string shortName, PfsArchive s3dArchive)
         {
             var wldFile = new WldFileZone(wldFileInArchive, shortName, WldType.Sky, logger, settings);
@@ -213,10 +216,10 @@ namespace LanternExtractor.EQ
         {
             var wldFile = new WldFileEquipment(wldFileInArchive, shortName, WldType.Equipment, logger, settings);
             var exportPath = rootFolder +
-                (settings.ExportEquipmentToSingleFolder &&
-                 settings.ModelExportFormat == ModelExportFormat.Intermediate
-                    ? "equipment/Textures/"
-                    : shortName + "/Textures/");
+                             (settings.ExportEquipmentToSingleFolder &&
+                              settings.ModelExportFormat == ModelExportFormat.Intermediate
+                                 ? "equipment/Textures/"
+                                 : shortName + "/Textures/");
 
             InitializeWldAndWriteTextures(wldFile, rootFolder, exportPath, s3dArchive, settings, logger);
         }

LanternExtractor/EQ/Wld/WldFile.cs
@@ -24,11 +24,6 @@ namespace LanternExtractor.EQ.Wld
         public WldType WldType => _wldType;
 
         /// <summary>
-        /// The link between fragment types and fragment classes
-        /// </summary>
-        private Dictionary<int, Func<WldFragment>> _fragmentBuilder;
-
-        /// <summary>
         /// A link of indices to fragments
         /// </summary>
         protected List<WldFragment> _fragments;
@@ -158,8 +153,6 @@ namespace LanternExtractor.EQ.Wld
             byte[] stringHash = reader.ReadBytes((int)stringHashSize);
 
             ParseStringHash(WldStringDecoder.DecodeString(stringHash));
-
-            long readPosition = 0;
 
             for (int i = 0; i < fragmentCount; ++i)
             {

LanternExtractor/Settings.cs
@@ -45,20 +45,11 @@ namespace LanternExtractor
         /// Sets the desired model export format
         /// </summary>
         public ModelExportFormat ModelExportFormat { get; private set; }
-
-        /// <summary>
-        /// Sets the desired model export format
-        /// </summary>
+        
         public bool ExportCharactersToSingleFolder { get; private set; }
-
-        /// <summary>
-        /// Sets the desired model export format
-        /// </summary>
+        
         public bool ExportEquipmentToSingleFolder { get; private set; }
-
-        /// <summary>
-        /// Export all sound files to a single folder
-        /// </summary>
+        
         public bool ExportSoundsToSingleFolder { get; private set; }
 
         /// <summary>
@@ -142,88 +133,88 @@ namespace LanternExtractor
                 return;
             }
 
-            if (parsedSettings.ContainsKey("EverQuestDirectory"))
+            if (parsedSettings.TryGetValue("EverQuestDirectory", out var setting))
             {
-                EverQuestDirectory = parsedSettings["EverQuestDirectory"];
+                EverQuestDirectory = setting;
 
                 // Ensure the path ends with a /
                 EverQuestDirectory = Path.GetFullPath(EverQuestDirectory + "/");
             }
 
-            if (parsedSettings.ContainsKey("RawS3DExtract"))
+            if (parsedSettings.TryGetValue("RawS3DExtract", out var parsedSetting))
             {
-                RawS3dExtract = Convert.ToBoolean(parsedSettings["RawS3DExtract"]);
+                RawS3dExtract = Convert.ToBoolean(parsedSetting);
             }
 
-            if (parsedSettings.ContainsKey("ExportZoneMeshGroups"))
+            if (parsedSettings.TryGetValue("ExportZoneMeshGroups", out var setting1))
             {
-                ExportZoneMeshGroups = Convert.ToBoolean(parsedSettings["ExportZoneMeshGroups"]);
+                ExportZoneMeshGroups = Convert.ToBoolean(setting1);
             }
 
-            if (parsedSettings.ContainsKey("ExportHiddenGeometry"))
+            if (parsedSettings.TryGetValue("ExportHiddenGeometry", out var parsedSetting1))
             {
-                ExportHiddenGeometry = Convert.ToBoolean(parsedSettings["ExportHiddenGeometry"]);
+                ExportHiddenGeometry = Convert.ToBoolean(parsedSetting1);
             }
 
-            if (parsedSettings.ContainsKey("ExportZoneWithObjects"))
+            if (parsedSettings.TryGetValue("ExportZoneWithObjects", out var setting2))
             {
-                ExportZoneWithObjects = Convert.ToBoolean(parsedSettings["ExportZoneWithObjects"]);
+                ExportZoneWithObjects = Convert.ToBoolean(setting2);
             }
 
-            if (parsedSettings.ContainsKey("ModelExportFormat"))
+            if (parsedSettings.TryGetValue("ModelExportFormat", out var parsedSetting2))
             {
-                var exportFormatSetting = (ModelExportFormat)Convert.ToInt32(parsedSettings["ModelExportFormat"]);
+                var exportFormatSetting = (ModelExportFormat)Convert.ToInt32(parsedSetting2);
                 ModelExportFormat = exportFormatSetting;
             }
 
-            if (parsedSettings.ContainsKey("ExportCharacterToSingleFolder"))
+            if (parsedSettings.TryGetValue("ExportCharacterToSingleFolder", out var setting3))
             {
-                ExportCharactersToSingleFolder = Convert.ToBoolean(parsedSettings["ExportCharacterToSingleFolder"]);
+                ExportCharactersToSingleFolder = Convert.ToBoolean(setting3);
             }
 
-            if (parsedSettings.ContainsKey("ExportEquipmentToSingleFolder"))
+            if (parsedSettings.TryGetValue("ExportEquipmentToSingleFolder", out var parsedSetting3))
             {
-                ExportEquipmentToSingleFolder = Convert.ToBoolean(parsedSettings["ExportEquipmentToSingleFolder"]);
-            }
-
-            if (parsedSettings.ContainsKey("ExportSoundsToSingleFolder"))
-            {
-                ExportSoundsToSingleFolder = Convert.ToBoolean(parsedSettings["ExportSoundsToSingleFolder"]);
-            }
-
-            if (parsedSettings.ContainsKey("ExportAllAnimationFrames"))
-            {
-                ExportAllAnimationFrames = Convert.ToBoolean(parsedSettings["ExportAllAnimationFrames"]);
-            }
-
-            if (parsedSettings.ContainsKey("ExportGltfVertexColors"))
-            {
-                ExportGltfVertexColors = Convert.ToBoolean(parsedSettings["ExportGltfVertexColors"]);
-            }
-
-            if (parsedSettings.ContainsKey("ExportGltfInGlbFormat"))
-            {
-                ExportGltfInGlbFormat = Convert.ToBoolean(parsedSettings["ExportGltfInGlbFormat"]);
-            }
-
-            if (parsedSettings.ContainsKey("ClientDataToCopy"))
-            {
-                ClientDataToCopy = parsedSettings["ClientDataToCopy"];
+                ExportEquipmentToSingleFolder = Convert.ToBoolean(parsedSetting3);
             }
             
-            if (parsedSettings.ContainsKey("ClientDataToCopy"))
+            if (parsedSettings.TryGetValue("ExportSoundsToSingleFolder", out var setting4))
             {
-                ClientDataToCopy = parsedSettings["ClientDataToCopy"];
-            }
-            
-            if (parsedSettings.ContainsKey("CopyMusic"))
-            {
-                CopyMusic = Convert.ToBoolean(parsedSettings["CopyMusic"]);
+                ExportSoundsToSingleFolder = Convert.ToBoolean(setting4);
             }
 
-            if (parsedSettings.ContainsKey("LoggerVerbosity"))
+            if (parsedSettings.TryGetValue("ExportAllAnimationFrames", out var parsedSetting4))
             {
-                LoggerVerbosity = Convert.ToInt32(parsedSettings["LoggerVerbosity"]);
+                ExportAllAnimationFrames = Convert.ToBoolean(parsedSetting4);
+            }
+
+            if (parsedSettings.TryGetValue("ExportGltfVertexColors", out var setting5))
+            {
+                ExportGltfVertexColors = Convert.ToBoolean(setting5);
+            }
+
+            if (parsedSettings.TryGetValue("ExportGltfInGlbFormat", out var parsedSetting5))
+            {
+                ExportGltfInGlbFormat = Convert.ToBoolean(parsedSetting5);
+            }
+
+            if (parsedSettings.TryGetValue("ClientDataToCopy", out var setting6))
+            {
+                ClientDataToCopy = setting6;
+            }
+            
+            if (parsedSettings.TryGetValue("ClientDataToCopy", out var parsedSetting6))
+            {
+                ClientDataToCopy = parsedSetting6;
+            }
+            
+            if (parsedSettings.TryGetValue("CopyMusic", out var setting7))
+            {
+                CopyMusic = Convert.ToBoolean(setting7);
+            }
+
+            if (parsedSettings.TryGetValue("LoggerVerbosity", out var parsedSetting7))
+            {
+                LoggerVerbosity = Convert.ToInt32(parsedSetting7);
             }
         }
     }

commit b8bda989fa72981636d45cdf1d330226d72fb84e

Fix unwanted loading of chequip Regression in https://github.com/LanternEQ/LanternExtractor/commit/9e67b3ca09589326f93967ea0a3baceeb1c91ff8

LanternExtractor/EqFileHelper.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -158,8 +158,14 @@ namespace LanternExtractor
 
         private static bool IsValidArchive(string archiveName)
         {
-            return archiveName.EndsWith(".s3d") || archiveName.EndsWith(".t3d") || archiveName.EndsWith(".pfs")
-                && !archiveName.Contains("chequip") && !archiveName.EndsWith("_lit.s3d");
+            // chequip contains broken/conflicting data.
+            // _lit archives get injected later during archive extraction
+            if (archiveName.Contains("chequip") || archiveName.EndsWith("_lit.s3d"))
+            {
+                return false;
+            }
+
+            return archiveName.EndsWith(".s3d") || archiveName.EndsWith(".t3d") || archiveName.EndsWith(".pfs");
         }
 
         private static bool IsZoneArchive(string archiveName)